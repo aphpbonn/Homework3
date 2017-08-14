1aaab6ee5478761ed68d8def47e4e535e0733762
Bonn <aphpbonn@gmail.com>
2017-08-15 00:15:34 +0700

    first commit

---
+++ app/src/main/java/com/example/tngp17_001/homework3/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.tngp17_001.homework3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    EditText editTextInput;
+    ImageButton buttonPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initInstances();
+    }
+    private void initInstances(){
+        editTextInput = (EditText) findViewById(R.id.edit_text_input);
+        buttonPlay = (ImageButton) findViewById(R.id.button_play);
+        buttonPlay.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(this, SecondActivity.class);
+        try {
+            int input = Integer.parseInt(editTextInput.getText().toString());
+            intent.putExtra("input",input);
+            startActivity(intent);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "invalid input",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tngp17_001/homework3/SecondActivity.java
@@ -0,0 +1,40 @@
+package com.example.tngp17_001.homework3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity implements View.OnClickListener {
+    int input = 0;
+    int count = 0;
+    ImageButton buttonCoin;
+    TextView tvDisplay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        input = intent.getIntExtra("input",0);
+        iniInstances();
+    }
+
+    private void iniInstances() {
+        tvDisplay = (TextView) findViewById(R.id.tv_display);
+        tvDisplay.setText(count+"");
+        buttonCoin = (ImageButton) findViewById(R.id.button_coin);
+        buttonCoin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(count >= input){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            tvDisplay.setText(++count+"");
+        }
+    }
+}
